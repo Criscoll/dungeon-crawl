5e0f2439ddd1a87f995c65324c4815e2f7b3a14c
ruiqi <ruiqili1024@gmail.com>
2019-11-02 15:07:30 +1100

    Player can now push boulder and add the test for it

--- src/unsw/dungeon/PlayerTest.java
+++ src/unsw/dungeon/PlayerTest.java
@@ -20,6 +20,8 @@ class PlayerTest {
 		player.attach(o1);
 		doorOpenHandler o2 = new doorOpenHandler(this.dungeon);
 		player.attach(o2);
+		boulderPushHandler o3 = new boulderPushHandler(this.dungeon);
+		player.attach(o3);
 		dungeon.setPlayer(player);
 	}
 
@@ -109,26 +111,81 @@ class PlayerTest {
 	}
 	
 	/**
-	 * player's movement should be blocked by a boulder and a wall
-	 * and the boulder can be pushed to next square if there is no wall 
-	 * and player should be in the previous square the boulder at
+	 * Acceptance Criteria:
+	 * Player should only be able to push one boulder at a time.
+  	 * Player should be able to push boulders into the adjacent square if that square does not have any other entities in it
+  	 * The enemies shouldn't be able to push the boulder, so it should act like a wall to them
 	 */
-//	@Test
-//	void testBoulder() {
-//		Wall wall = new Wall(2, 0);
-//		this.dungeon.addEntity(wall);
-//		Boulder boulder = new Boulder(1, 0);
-//		this.dungeon.addEntity(boulder);
-//		boulder = new Boulder(0, 1);
-//		this.dungeon.addEntity(boulder);
-//		player.moveRight();
-//		assertTrue(player.getX() == 0);
-//		assertTrue(player.getY() == 0);
-//		player.moveDown();
-//		assertTrue(player.getX() == 0);
-//		assertTrue(player.getY() == 1);
-//		assertTrue(boulder.getX() == 0);
-//		assertTrue(boulder.getY() == 2);
-//	}
+	@Test
+	void testBoulder() {
+		/**
+		 * player's movement should be blocked by a boulder and a wall
+		 * and the boulder can be pushed to next square if there is no wall 
+		 * and player should be in the previous square the boulder at
+		 */
+		Wall wall = new Wall(2, 0);
+		this.dungeon.addEntity(wall);
+		Boulder boulder = new Boulder(1, 0);
+		this.dungeon.addEntity(boulder);
+		boulder = new Boulder(0, 1);
+		this.dungeon.addEntity(boulder);
+		player.moveRight();
+		assertEquals(player.getX(), 0);
+		assertEquals(player.getY(), 0);
+		player.moveDown();
+		assertEquals(player.getX(), 0);
+		assertEquals(player.getY(), 1);
+		assertEquals(boulder.getX(), 0);
+		assertEquals(boulder.getY(), 2);
+
+		//Player should only be able to push one boulder at a time.
+		boulder = new Boulder(0, 3);
+		this.dungeon.addEntity(boulder);
+		player.moveDown();
+		assertEquals(player.getX(), 0);
+		assertEquals(player.getY(), 1);
+		assertTrue(this.dungeon.getEntity(0, 2) instanceof Boulder);
+		assertEquals(boulder.getX(), 0);
+		assertEquals(boulder.getY(), 3);
+	}
+	
+	@Test
+	void testBoulderAllDirections() {
+		Boulder boulder = new Boulder(1, 1);
+		this.dungeon.addEntity(boulder);
+		// boulder move down
+		player.moveRight();
+		player.moveDown();
+		assertEquals(1, player.getX());
+		assertEquals(1, player.getY());
+		assertEquals(1, boulder.getX());
+		assertEquals(2, boulder.getY());
+		//boulder move right
+		player.moveLeft();
+		player.moveDown();
+		player.moveRight();
+		assertEquals(1, player.getX());
+		assertEquals(2, player.getY());
+		assertEquals(2, boulder.getX());
+		assertEquals(2, boulder.getY());
+		//boulder move up
+		player.moveDown();
+		player.moveRight();
+		player.moveUp();
+		assertEquals(2, player.getX());
+		assertEquals(2, player.getY());
+		assertEquals(2, boulder.getX());
+		assertEquals(1, boulder.getY());
+		//boulder move left
+		player.moveRight();
+		player.moveUp();
+		player.moveLeft();
+		assertEquals(2, player.getX());
+		assertEquals(1, player.getY());
+		assertEquals(1, boulder.getX());
+		assertEquals(1, boulder.getY());	
+	}
+	
+		
 	
 }
---
+++ src/unsw/dungeon/boulderPushHandler.java
@@ -0,0 +1,46 @@
+package unsw.dungeon;
+
+public class boulderPushHandler implements MovementObserver{
+
+	private Dungeon dungeon;
+	public boulderPushHandler(Dungeon dungeon) {
+		this.dungeon = dungeon;
+	}
+	
+	@Override
+	public void update(int x, int y, Player player) {
+		Entity e = dungeon.getEntity(x, y);
+		if(e == null) return;
+		// player tried to move to the location of the boulder
+		if(e instanceof Boulder) {
+			//boulder above the player
+			if(x == player.getX() && y == player.getY()-1) {
+				// only push the boulder when there is no entity in the square
+				if(e.getY() > 0 && dungeon.getEntity(x, y-1) == null) {
+					e.y().set(e.getY()-1);
+				}
+			}
+			//boulder below the player
+			else if(x == player.getX() && y == player.getY()+1) {
+				if(e.getY() < dungeon.getHeight() - 1 && dungeon.getEntity(x, y+1) == null) {
+					e.y().set(e.getY()+1);
+				}
+			}
+				
+			//boulder on the left side of the player
+			else if(x == player.getX() - 1 && y == player.getY()) {
+				if(e.getX() > 0 && dungeon.getEntity(x-1, y) == null) {
+					e.x().set(e.getX()-1);
+				}
+			}
+			
+			//boulder on the right side of the player
+			else if(x == player.getX() + 1 && y == player.getY()){
+				if(e.getX() < dungeon.getWidth() - 1 && dungeon.getEntity(x+1, y) == null) {
+					e.x().set(e.getX()+1);
+				}
+			}
+		}
+	}
+
+}
