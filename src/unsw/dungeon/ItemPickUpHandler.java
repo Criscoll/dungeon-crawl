4dbd60194bfdeccf5313e68bc5727c1682916d24
Cristian2022 <cristiand2021@gmail.com>
2019-11-17 15:09:46 +1100

    sound files added not implemented

--- src/unsw/dungeon/ItemPickUpHandler.java
+++ src/unsw/dungeon/ItemPickUpHandler.java
@@ -1,5 +1,10 @@
 package unsw.dungeon;
 
+import java.io.File;
+
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+
 public class ItemPickUpHandler implements MovementObserver{
 
 	private Dungeon dungeon;
@@ -22,11 +27,22 @@ public class ItemPickUpHandler implements MovementObserver{
 			}
 			if (e instanceof Sword) {
 				player.setSword(true); // player now has a sword
+				Media sound = new Media(new File("sounds/sword_draw.mp3").toURI().toString());
+				MediaPlayer mediaPlayer = new MediaPlayer(sound);
+				mediaPlayer.play();
 				
 			}
 			if (e instanceof Potion) {
 				player.setInvinicibility(false); // if the player is already invincible then the cooldown timer should reset
 				player.setInvinicibility(true); // player is now invincible
+				
+				
+			}
+			
+			if (e instanceof Treasure || e instanceof Key) {
+				Media sound = new Media(new File("sounds/treasure_pickup.mp3").toURI().toString());
+				MediaPlayer mediaPlayer = new MediaPlayer(sound);
+				mediaPlayer.play();
 			}
 			
 			dungeon.removeEntity(e);
